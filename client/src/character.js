0ba7eb671426a35973251eab740437a1510a3421
harutea <snowhanami@gmail.com>
2023-07-28 10:48:47 +0900

    update Character: initialize direction vector

--- client/src/character.js
+++ client/src/character.js
@@ -2,9 +2,26 @@ import React, { useEffect, useRef } from 'react'
 import { useFrame, useThree } from '@react-three/fiber'
 import { useSphere } from '@react-three/cannon'
 import * as THREE from 'three'
+import Control from './control'
 
 const Character = (props) => {
-  return <></>
+  const { forward, backward, left, right } = Control()
+  const direction = new THREE.Vector3()
+
+  useFrame(() => {
+    direction.set(
+      Number(forward) - Number(backward),
+      0,
+      Number(left) - Number(right)
+    )
+  })
+
+  return (
+    <mesh>
+      <sphereGeometry />
+      <meshStandardMaterial />
+    </mesh>
+  )
 }
 
 export default Character
